d36dcad9491852f5361c7865f11dc1c25ac2d93f
the-best-developer <im.dylan.gosnell@gmail.com>
2019-06-25 22:32:40 -0400

    Redux Swapi MVP

--- src/views/CharacterListView.js
+++ src/views/CharacterListView.js
@@ -2,7 +2,9 @@ import React from "react";
 import { connect } from "react-redux";
 
 import { CharacterList } from "../components";
+
 // import actions
+import getPeople from '../actions'
 
 class CharacterListView extends React.Component {
   constructor() {
@@ -11,12 +13,20 @@ class CharacterListView extends React.Component {
 
   componentDidMount() {
     // call our action
+    this.props.getPeople();
   }
 
   render() {
+    console.log(this.props)
     if (this.props.fetching) {
       // return something here to indicate that you are fetching data
+      return <div><h1>???????</h1></div>
     }
+    else if (this.props.error) {
+      // return something here to indicate that you are fetching data
+      return <div><h1>{this.props.error}</h1></div>
+    }
+    
     return (
       <div className="CharactersList_wrapper">
         <CharacterList characters={this.props.characters} />
@@ -25,11 +35,18 @@ class CharacterListView extends React.Component {
   }
 }
 
+
 // our mapStateToProps needs to have two properties inherited from state
-// the characters and the fetching boolean
-export default connect(
-  null /* mapStateToProps replaces null here */,
-  {
-    /* action creators go here */
+// // the characters and the fetching boolean
+// export default connect( mapStateToProps, getPeople )(CharacterListView);
+
+const mapStateToProps = (state) => {
+  console.log(state)
+  return {
+      characters: state.charsReducer.characters,
+      fetching: state.charsReducer.fetching,
+      error: state.charsReducer.error
   }
-)(CharacterListView);
+};
+
+export default connect(mapStateToProps, { getPeople } )(CharacterListView);
